35cba326e1293033f74096083f9fd83ccc833349
Anton Gorodishenin <a.gorodishenin@latera.ru>
2013-03-16 21:02:15 +0400

    Lab5 Добавлена запись результатов в файл. Многопроцессность отключена

--- lab5/main.c
+++ lab5/main.c
@@ -28,35 +28,38 @@ void start_worker(int i)
 
 	memset(pathname, '\0', strlen(pathname));
 	memset(range, '\0', strlen(range));
+	memset(c_top_limit, '\0', strlen(c_top_limit));
+	memset(c_low_limit, '\0', strlen(c_low_limit));
 
-	strcat(pathname,"/tmp/worker");
+	strcat(pathname,"worker\0");
 	sprintf(p_number,"%d",i);
 	strcat(pathname,p_number);
 
-	fifofd = open(&pathname,O_RDWR);
+	fifofd = open(pathname,O_RDWR);
 
-	read(fifofd, range, strlen(range));
+	read(fifofd, range, 40);
 
 	primes = malloc(sizeof(char)*atoi(range));
 	memset(primes, '\0', strlen(primes));
 
 	for(i=0;i<strlen(range);i++)
 	{
-		if(range[i]!=":")
+		if(range[i]==':')
 		{
 			n_top_low_flag = 1;
+			i++;
 		}
 		if(n_top_low_flag==0)
 		{
-			strcat(c_low_limit,range[i]);
+			strncat(c_low_limit,&range[i],1);
 		}
 		else
 		{
-			strcat(c_top_limit,range[i]);
+			strncat(c_top_limit,&range[i],1);
 		}
 	}
 
-	for(i=atoi(c_low_limit);i<atoi(c_top_limit);i++)
+	for(i=1;i<=atoi(c_top_limit);i++)
 	{
 		int j;
 		int dividers=0;
@@ -67,13 +70,19 @@ void start_worker(int i)
 				dividers++;
 			}
 		}
-		if(dividers==2)
+		if(dividers==0)
 		{
 			char temp_char[10];
 			sprintf(temp_char,"%d\n\0",i);
 			strcat(primes,temp_char);
 		}
 	}
+
+	int tmpfd = open("temp.log", O_RDWR);
+
+	write(tmpfd, primes, strlen(primes));
+
+	close(tmpfd);
 
 	write(fifofd,primes,strlen(primes));
 }
@@ -104,9 +113,7 @@ int main(int argc, char ** argv)
 	int i=1;
 
 	for(i=0;i<strlen(argv[4]);i++)
-	{
-		printf("asdasd");
-		
+	{		
 		if(argv[4][i]!=':')
 		{
 			if(n_top_low_flag==0)
@@ -142,22 +149,36 @@ int main(int argc, char ** argv)
 
 		char range[40];
 		char temp_string[10];
+		char p_number[4];
 
 		memset(range, '\0', strlen(range));
 		memset(temp_string, '\0', strlen(temp_string));
 		memset(pathname, '\0', strlen(pathname));
+		memset(p_number, '\0', strlen(p_number));
 
 		strcat(pathname,"worker\0");
 		
-		strcat(pathname,&i);
+		sprintf(p_number,"%d", i);
+
+		strcat(pathname,p_number);
 		
-		mkfifo(&pathname, O_RDWR);
+		if (mkfifo(&pathname, S_IRUSR|S_IWUSR))
+		{
+			printf("Can not create FIFO");
+			return 1;
+		}
 
 		processes[i].p_low_limit = ((n_top_limit-residue)/atoi(argv[2]))*i;
 
 		processes[i].p_top_limit = ((n_top_limit-residue)/atoi(argv[2]))+processes[i].p_low_limit;
 
-		processes[i].fifofd = open(&pathname,O_RDWR);
+		processes[i].fifofd = open(pathname,O_RDWR);
+
+		if(processes[i].fifofd==-1)
+		{
+			printf("Could not open named pipe");
+			return 1;
+		}
 
 		sprintf(temp_string, ":%d", processes[i].p_top_limit);
 
@@ -168,20 +189,31 @@ int main(int argc, char ** argv)
 		write(processes[i].fifofd,range,strlen(range));
 		
 		processes[i].pid = fork();
-		
-		if(getppid()!=0)
+
+		if(processes[i].pid==0)
 		{
 			start_worker(i);
+			return 0;
+		}
+		else
+		{
+			printf("created new proccess with id %d\n", processes[i].pid );
 		}
 	}
 
-	for(i=0;i<argv[2];i++)
+	for(i=0;i<atoi(argv[2])&&processes[i].pid;i++)
 	{
 		char buff[1000];
+		char pathname[20];
 		memset(buff,'\0',strlen(buff));
+		memset(pathname, '\0', strlen(pathname));
+		printf("wait to complete pid %d\n", processes[i].pid );
 		waitpid(processes[i].pid,NULL,NULL);
 		read(processes[i].fifofd,buff,strlen(buff));
 		printf(buff);
+		strcpy(pathname,"worker\0");
+		sprintf(pathname,"%d",i);
+		unlink(pathname);
 		kill(processes[i].pid, SIGINT);
 	}
 
