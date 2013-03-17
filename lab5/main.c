96950af6fc05070ab194c31f6cd261da9bf29d48
Anton Gorodishenin <a.gorodishenin@latera.ru>
2013-03-17 16:54:03 +0400

    Lab5 Поправлены баги. Стабильная версия

--- lab5/main.c
+++ lab5/main.c
@@ -14,16 +14,17 @@
 #include "args.h"
 #include <fcntl.h>
 #include <sys/stat.h>
+#include <math.h>
 
-void start_worker(int i)
+void start_worker(long i)
 {
 	char pathname[40];
-	int  fifofd;
+	long  fifofd;
 	char p_number[4];
 	char range[40];
 	char c_low_limit[20];
 	char c_top_limit[20];
-	int  n_top_low_flag=0;
+	long  n_top_low_flag=0;
 	char * primes;
 
 	memset(pathname, '\0', strlen(pathname));
@@ -39,7 +40,7 @@ void start_worker(int i)
 
 	read(fifofd, range, 40);
 
-	primes = malloc(sizeof(char)*atoi(range));
+	primes = malloc(sizeof(char)*atoi(range)*30);
 	memset(primes, '\0', strlen(primes));
 
 	for(i=0;i<strlen(range);i++)
@@ -59,45 +60,48 @@ void start_worker(int i)
 		}
 	}
 
-	for(i=1;i<=atoi(c_top_limit);i++)
+	for(i=atol(c_low_limit);i<=atol(c_top_limit);i++)
 	{
-		int j;
-		int dividers=0;
+		long j;
+		long dividers=0;
+		// to optimize uncomment three lines and comment lines "for"
+		// long max_i;
+		// max_i = pow(i,0.5);
+		// for(j=1;j<max_i;j++)
 		for(j=1;j<i;j++)
 		{
 			if((i%j==0)&&(j!=1)&&(j!=i))
 			{
 				dividers++;
+				//to optimize uncomment line ""
+				// break;
 			}
 		}
 		if(dividers==0)
 		{
+			printf("%d\n", i);
 			char temp_char[10];
 			sprintf(temp_char,"%d\n\0",i);
 			strcat(primes,temp_char);
 		}
 	}
 
-	int tmpfd = open("temp.log", O_RDWR);
-
-	write(tmpfd, primes, strlen(primes));
-
-	close(tmpfd);
+	primes[i+1]='\0';
 
 	write(fifofd,primes,strlen(primes));
 }
 
-int main(int argc, char ** argv)
+long main(long argc, char ** argv)
 {
 	
 	char    c_low_limit[10];
 	char    c_top_limit[10];
-	int     n_low_limit;
-	int    	n_top_limit;
-	int 	n_top_low_flag=0;//only to parse incoming parameters
-	int 	residue;
+	long     n_low_limit;
+	long    	n_top_limit;
+	long 	n_top_low_flag=0;//only to parse incoming parameters
+	long 	residue;
 
-	struct Processes * processes;
+	struct Processes processes[10];
 
 	memset(c_low_limit, '\0', strlen(c_low_limit));
 	memset(c_top_limit, '\0', strlen(c_top_limit));
@@ -108,9 +112,7 @@ int main(int argc, char ** argv)
 		return 1;
 	}
 
-	processes = malloc(sizeof(struct Processes)*atoi(argv[2]));
-
-	int i=1;
+	long i=1;
 
 	for(i=0;i<strlen(argv[4]);i++)
 	{		
@@ -168,9 +170,9 @@ int main(int argc, char ** argv)
 			return 1;
 		}
 
-		processes[i].p_low_limit = ((n_top_limit-residue)/atoi(argv[2]))*i;
+		processes[i].p_low_limit = ((n_top_limit-residue-n_low_limit)/atoi(argv[2]))*i+n_low_limit;
 
-		processes[i].p_top_limit = ((n_top_limit-residue)/atoi(argv[2]))+processes[i].p_low_limit;
+		processes[i].p_top_limit = ((n_top_limit-residue-n_low_limit)/atoi(argv[2]))+processes[i].p_low_limit;
 
 		processes[i].fifofd = open(pathname,O_RDWR);
 
@@ -187,6 +189,8 @@ int main(int argc, char ** argv)
 		strcat(range, temp_string);
 
 		write(processes[i].fifofd,range,strlen(range));
+
+		processes[i].p_number = i;
 		
 		processes[i].pid = fork();
 
@@ -198,23 +202,25 @@ int main(int argc, char ** argv)
 		else
 		{
 			printf("created new proccess with id %d\n", processes[i].pid );
+			printf("range: %s\n", range);
 		}
 	}
 
-	for(i=0;i<atoi(argv[2])&&processes[i].pid;i++)
+	for(i=0;(i<atoi(argv[2]))&&processes[0].pid;i++)
 	{
-		char buff[1000];
+		char p_number[5];
+		char buff[100000];
 		char pathname[20];
 		memset(buff,'\0',strlen(buff));
 		memset(pathname, '\0', strlen(pathname));
 		printf("wait to complete pid %d\n", processes[i].pid );
-		waitpid(processes[i].pid,NULL,NULL);
-		read(processes[i].fifofd,buff,strlen(buff));
+		waitpid(processes[i].pid,NULL,NULL);		
+		read(processes[i].fifofd,buff,100000 );
 		printf(buff);
 		strcpy(pathname,"worker\0");
-		sprintf(pathname,"%d",i);
+		sprintf(p_number,"%d",i);
+		strcat(pathname, p_number);
 		unlink(pathname);
-		kill(processes[i].pid, SIGINT);
 	}
 
 	return 0;
